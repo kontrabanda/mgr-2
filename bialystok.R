17f242d0efe3cc71077057958e5f29d88fe94cc8
kontrabanda <tomaszrydzewski92@gmail.com>
2018-01-25 20:59:52 +0100

    add model processing scripts

--- bialystok.R
+++ bialystok.R
@@ -3,10 +3,38 @@ source(file="./data/BialystokData.R")
 
 bialystokData <- BialystokData()
 
+### Logistic Regression
+crossValidation <- CrossValidation(bialystokData, LogisticRegressionModel)
+crossValidation$crossValidation()
+
+binaryRating <- BinaryRating(bialystokData, LogisticRegressionModel)
+binaryRating$computeRating()
+
 ### Naive Bayes
 crossValidation <- CrossValidation(bialystokData, NaiveBayesModel)
 crossValidation$crossValidation()
 
 binaryRating <- BinaryRating(bialystokData, NaiveBayesModel)
+binaryRating$computeRating()
+
+### kNN
+crossValidation <- CrossValidation(bialystokData, KNNModel)
+crossValidation$crossValidation()
+
+binaryRating <- BinaryRating(bialystokData, KNNModel)
+binaryRating$computeRating()
+
+### Random forest
+crossValidation <- CrossValidation(bialystokData, RandomForestModel)
+crossValidation$crossValidation()
+
+binaryRating <- BinaryRating(bialystokData, RandomForestModel)
+binaryRating$computeRating()
+
+### SVM
+crossValidation <- CrossValidation(bialystokData, SVMModel)
+crossValidation$crossValidation()
+
+binaryRating <- BinaryRating(bialystokData, SVMModel)
 binaryRating$computeRating()
 
--- bournemouth.R
+++ bournemouth.R
@@ -1,2 +1,41 @@
 source(file="./util.R")
 source(file="./data/BournemouthData.R")
+
+bournemouthData <- BournemouthData()
+
+### Logistic Regression
+crossValidation <- CrossValidation(bournemouthData, LogisticRegressionModel)
+crossValidation$crossValidation()
+
+binaryRating <- BinaryRating(bournemouthData, LogisticRegressionModel)
+binaryRating$computeRating()
+
+### Naive Bayes
+crossValidation <- CrossValidation(bournemouthData, NaiveBayesModel)
+crossValidation$crossValidation()
+
+binaryRating <- BinaryRating(bournemouthData, NaiveBayesModel)
+binaryRating$computeRating()
+
+### kNN
+crossValidation <- CrossValidation(bournemouthData, KNNModel)
+crossValidation$crossValidation()
+
+binaryRating <- BinaryRating(bournemouthData, KNNModel)
+binaryRating$computeRating()
+
+### Random forest
+crossValidation <- CrossValidation(bournemouthData, RandomForestModel)
+crossValidation$crossValidation()
+
+binaryRating <- BinaryRating(bournemouthData, RandomForestModel)
+binaryRating$computeRating()
+
+### SVM
+crossValidation <- CrossValidation(bournemouthData, SVMModel)
+crossValidation$crossValidation()
+
+binaryRating <- BinaryRating(bournemouthData, SVMModel)
+binaryRating$computeRating()
+
+
