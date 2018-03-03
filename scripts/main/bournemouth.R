4f1e934a32930f82516ae5b87bb90c560cd36abd
kontrabanda <tomaszrydzewski92@gmail.com>
2018-03-03 11:29:26 +0100

    test

--- scripts/main/bournemouth.R
+++ scripts/main/bournemouth.R
@@ -1,7 +1,10 @@
 library(methods) # for Rscript
 
 #source(file="./util.R")
+source(file="./const.R")
 source(file="./model/NaiveBayesModel.R")
+source(file="./strategy/TimeCrossValidation.R")
+source(file="./rating/BinaryRating.R")
 
 source(file="./data/bournemouth/BournemouthData.R")
 source(file="./data/bournemouth/BournemouthDataWithoutDuplicates.R")
