ded22fba4491fa1d50832d28e17c569cf3b5d11d
kontrabanda <tomaszrydzewski92@gmail.com>
2018-03-04 20:21:18 +0100

    Change knn implementation

--- model/KNNModel.R
+++ model/KNNModel.R
@@ -1,4 +1,5 @@
-library(kknn)
+#library(kknn)
+library('class')
 source(file="./model/ClassificationModelBase.R")
 
 KNNModel <- setRefClass(
@@ -14,8 +15,37 @@ KNNModel <- setRefClass(
       trainData <<- data
     },
     predictLabels = function(testData) {
-      results <- kknn(formula = label~., trainData, testData, k = 10)
-      results$prob
+      #results <- kknn(formula = label~., trainData, testData, k = 10)
+      #results$prob
+      trainWithoutLabel <- trainData[,-which(names(trainData) == "label")]
+      trainWithoutLabel <- sapply(trainWithoutLabel, function(x) as.numeric(x))
+      testData <- sapply(testData, function(x) as.numeric(x))
+      
+      res <- knn(trainWithoutLabel, testData, factor(trainData$label), k = 3, prob=TRUE)
+      #attributes(res)$prob
+      
+      #predRes <- knn$predictLabels(test)
+      prob <- as.numeric(attributes(res)$prob)
+      
+      resultWithWinningProb <- data.frame(label = res, prob = prob)
+      
+      temp <- apply(resultWithWinningProb, 1 , function(x) {
+        res <- c()
+        prob <- as.numeric(x['prob'])
+        if(x['label'] == 0) {
+          res[1] <- prob
+          res[2] <- 1 - prob
+        } else {
+          res[1] <- 1 - prob
+          res[2] <- prob
+        }
+        
+        names(res) <- c('0', '1')
+        res
+      })
+      
+      temp <- t(temp)
+      data.frame(X0=temp[,1], X1=temp[,2])
     }
   ),
   contains=c("ClassificationModelBase")
--- scripts/main/bialystok.R
+++ scripts/main/bialystok.R
@@ -4,14 +4,46 @@ source(file="./data/bialystok/BialystokDataWithoutDuplicates.R")
 source(file="./data/bialystok/BialystokPOIDistData.R")
 source(file="./data/bialystok/BialystokPOIDensData.R")
 
-bialystokData <- BialystokData()
+#bialystokData <- BialystokData()
 bialystokData <- BialystokDataWithoutDuplicates()
-bialystokData <- BialystokPOIDistData()
-bialystokData <- BialystokPOIDensData('r100')
+#bialystokData <- BialystokPOIDistData()
+#bialystokData <- BialystokPOIDensData('r100')
 methodName <- 'time-cross-validation'
 methodName <- 'test2'
 
 
+train <- bialystokData$getData('CHU')[1:100,]
+test <- bialystokData$getTestData()[200:300,]
+
+nb <- NaiveBayesModel()
+nb$trainModel(train)
+res <- nb$predictLabels(test)
+
+knn <- KNNModel()
+knn$trainModel(train)
+
+predRes <- knn$predictLabels(test)
+prob <- as.numeric(attributes(predRes)$prob)
+
+resultWithWinningProb <- data.frame(label = predRes, prob = prob)
+
+temp <- apply(resultWithWinningProb, 1 , function(x) {
+  res <- c()
+  prob <- as.numeric(x['prob'])
+  if(x['label'] == 0) {
+    res[1] <- prob
+    res[2] <- 1 - prob
+  } else {
+    res[1] <- 1 - prob
+    res[2] <- prob
+  }
+
+  names(res) <- c('0', '1')
+  res
+})
+
+temp <- t(temp)
+data.frame(X0=temp[,1], X1=temp[,2])
 
 ### Logistic Regression
 crossValidation <- TimeCrossValidation(methodName, bialystokData, LogisticRegressionModel)
