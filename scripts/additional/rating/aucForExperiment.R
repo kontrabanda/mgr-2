021ebe5cae5c0c743690f5621d89a0ae65c134b6
kontrabanda <tomaszrydzewski92@gmail.com>
2018-03-14 00:57:27 +0100

    add script to gather aucs for experiment

---
+++ scripts/additional/rating/aucForExperiment.R
@@ -0,0 +1,63 @@
+experimentName <- 'norm-3'
+
+resultsPath <- paste('../results', experimentName, sep = '/')
+ratingResultsPath <- paste('../ratings', experimentName, sep = '/')
+
+dir.create(ratingResultsPath)
+
+getMethodNameFromPath <- function(path) {
+  gsub('.*/(.*)/rating/auc.csv', '\\1', path)
+}
+
+getAucsFromPaths <- function(dataSourceAucPaths) {
+  methodNames <- c('category')
+  results <- NULL
+  
+  for(singleAucPath in dataSourceAucPaths) {
+    methodNames <- c(methodNames, getMethodNameFromPath(singleAucPath))
+    methodAuc <- read.csv(file = singleAucPath)
+    
+    if(is.null(results)) {
+      results <- methodAuc[, c('category', 'value')]
+    } else {
+      results <- cbind(results, methodAuc[, c('value')])
+    }
+  }
+  
+  names(results) <- methodNames
+  results
+}
+
+formatAucs <- function(aucs) {
+  result <- round(aucs[, !names(aucs) %in% c("category")], digits = 4)
+  cbind(category = aucs$category, result)
+}
+
+getDataSourceNameFromPath <- function(path) {
+  regExp <- paste(resultsPath, '.*/(.*)$', sep = '')
+  gsub(regExp, '\\1', path)
+}
+
+saveAucs <- function(dataSourceName, aucs) {
+  fileName <- paste(dataSourceName, 'Auc', '.csv', sep = '')
+  path <- paste(ratingResultsPath, fileName, sep = '/')
+  write.csv(file = path, x = aucs)
+}
+   
+computeDataSourceAUC <- function(dataSourceDir) {
+  dataSourceAucPaths <- list.files(dataSourceDir, patter = "auc.csv$", recursive = T, full.names = T)
+  dataSourceName <- getDataSourceNameFromPath(dataSourceDir)
+  aucs <- getAucsFromPaths(dataSourceAucPaths)
+  aucs <- formatAucs(aucs)
+  saveAucs(dataSourceName, aucs)
+}
+
+dataSourcesDirs <- list.dirs(resultsPath, recursive = F, full.names = T)
+
+for(singleDir in dataSourcesDirs) {
+  computeDataSourceAUC(singleDir)
+}
+
+
+
+
