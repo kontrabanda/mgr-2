c66d70f45119e6cdc2e8f4216a478d2b5d07fbc4
kontrabanda <tomaszrydzewski92@gmail.com>
2018-04-14 22:05:54 +0200

    params

--- params/bournemouthParams.R
+++ params/bournemouthParams.R
@@ -2,7 +2,7 @@ source(file="./params/InputParams.R")
 
 inputParams <- InputParams()
 inputParams$dataName <- 'bournemouth_poi_dens_w_geo'
-inputParams$methodName <- 'knn'
+inputParams$methodName <- 'svm'
 inputParams$monthInterval <- 3
 inputParams$fromYear <- 2016
 inputParams$poiRadius <- 200
